7400135d3816d2f6e07c4e8b4b3e3387cc6a8a1c
ab3nxy <abenxy0@gmail.com>
2023-09-29 13:36:32 -0400

    Web deployment of the application using Flask

--- server.py
+++ server.py
@@ -7,13 +7,13 @@ app = Flask("Emotion Detection")
 def emo_detector():
     text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
-    anger = ['anger_score']
-    disgust = ['disgust_score']
-    fear = ['fear_score']
-    joy = ['joy_score']
-    sadness = ['sadness_score']
-    dominant_emotion_name = ['dominant_emotion_name']
-    return f"For the given statement, the system response is 'anger': {anger}, 'disgust': {disgust}, 'fear': {fear}, 'joy': {joy} and 'sadness': {sadness}. The dominant emotion is {dominant_emotion_name}."
+    anger = response['anger'],
+    disgust = response['disgust'],
+    fear = response['fear']
+    joy = response['joy']
+    sadness = response['sadness']
+    dominant_emotion = response['dominant_emotion']
+    return f"For the given statement, the system response is 'anger': {anger}, 'disgust': {disgust}, 'fear': {fear}, 'joy': {joy} and 'sadness': {sadness}. The dominant emotion is {dominant_emotion}."
 
 @app.route("/")
 def render_index_page():
